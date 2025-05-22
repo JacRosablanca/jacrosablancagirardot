3ff931a2c81ff18cdf440a935057b76250e56c92
Ankamo <102838840+Ankamo@users.noreply.github.com>
2025-05-22 16:34:26 -0500

    Update App V. 1.0.0.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/footer/Footer";
 import NavBar from "@/components/nav/NavBar";
 
 export default function Home() {
-  const [dark, setDark] = useState(true);
+  const [dark] = useState(true);
 
   return (
     <div className={`${dark ? "bg-[#141a2e]" : "bg-gradient-to-br from-white via-blue-100 to-lime-100"} min-h-screen flex flex-col transition-colors duration-500`}>
