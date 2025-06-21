a8f2419d483c734c8156f3ee13e5f9949ea24e0e
Ankamo <102838840+Ankamo@users.noreply.github.com>
2025-06-20 23:40:13 -0500

    Create App V. 1.0.0.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
 "use client";
-import React, { Component } from 'react'
 import Header from "@/components/header/page";
 import Footer from "@/components/footer/page";
 
-export class page extends Component {
-  render() {
-    return (
-      <div className="min-h-screen flex flex-col bg-[#f5f6fa] dark:bg-[#18181b] transition-colors duration-300">
-        <Header />
-        <main className="flex-1 flex flex-col items-center justify-center px-4 py-8">
-          
-        </main>
-        <Footer />
-      </div>
-    )
-  }
+export default function Page() {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#f5f6fa] dark:bg-[#18181b] transition-colors duration-300">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center px-4 py-8">
+        
+      </main>
+      <Footer />
+    </div>
+  );
 }
-
-export default page
